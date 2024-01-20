b6bbcb5e429dd18b45c0e0fc496ed8c84277a320
jagadeesh kumar s <109891560+Jagadeesh825@users.noreply.github.com>
2024-01-20 16:58:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
    
-<div className="App" >
-
+    <div className="App" style={{ background: 'linear-gradient(to right, #111d2e, #050c2d)' }}>
   
 <div class="area" >
 <ul class="circles">
